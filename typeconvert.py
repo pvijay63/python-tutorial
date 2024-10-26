4517ba73de7507c34ec4b3a9461c31781da20ebf
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-10-26 17:06:19 +0530

    converting one datatype into other datatype

---
+++ typeconvert.py
@@ -0,0 +1,13 @@
+#int(),bool(),str(),float()
+#implicit type conversion----no data lose while data conversion
+a=6
+print(str(a))
+print(int(a))
+print(float(a))
+print(bool(a))
+#explicit type conversion --data lose while conversion
+b=9.5
+print(int(b))
+print(float(b))
+print(str(b))
+print(bool(b))
