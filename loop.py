184f5f558beaa642314e7bcf4b7260a9609c59a6
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-10-26 17:10:37 +0530

    loop statements in python

---
+++ loop.py
@@ -0,0 +1,23 @@
+#loops for and while
+for i in range(0,10):
+    print("Helo ")
+#ex
+a=[2,-23,4.7]
+for j in a:
+    print(a[0]) 
+#ex 
+c="helo"
+for l in c:
+    print(l)
+#while
+y=3
+while y>2:
+    print("h")
+    
+#ex
+x=9
+while x<12:
+    print("c")
+    x+=1
+
+
