41893e03d023ddc7786a8342390d4bcba39a3187
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-10-26 17:13:14 +0530

    jumping statements in python

---
+++ jumpst.py
@@ -0,0 +1,24 @@
+#break,continue,pass
+#jump statements or loop control statements
+#break--terminate
+a="good"
+for i in a:
+    if i=='d':
+        break
+    print(i)  
+#continue---skip
+b="life"
+for i in b:
+    if i =='i':
+        continue
+    print(i)
+#pass---ignore
+c="various"
+for i in c:
+    if i=='v':
+        pass
+    print(i)
+
+
+
+
