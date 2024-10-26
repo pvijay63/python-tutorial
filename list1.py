8e198ff64a5a9837232c2eefb863c0e4ddd2f382
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-10-26 17:13:55 +0530

    list datatype in python

---
+++ list1.py
@@ -0,0 +1,36 @@
+'''
+list is mutable data  type
+store different type elements
+allow duplicates
+allow indexing
+'''
+#methds of list
+'''
+append
+count
+insert
+extend
+remove
+pop
+count
+index
+slicing
+'''
+a=[1,2,3,6,8]
+print(a[2])
+print(a[0:3])
+print(a[0:4:2])
+a.append(9)
+print(a)
+a.extend([7])
+print(a)
+a.remove(1)
+print(a)
+a.pop()
+print(a)
+print(a[2])
+print(a.count(3))
+print(a.index(3))
+a.insert(67,"hi")
+print(a)
+
