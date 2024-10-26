510eb856cd342089f0779b24d4577c229e3324ce
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-10-26 17:11:29 +0530

    conditional statements in python

---
+++ condition.py
@@ -0,0 +1,34 @@
+a=3
+if  a>2:
+    print("ihg")
+else :
+    print("good")
+
+#nested if else
+n=5
+if  n==5:
+    print("4")
+elif n>0:
+    print("hi")
+elif  n<1 & n<0 :
+    print("ko")
+else : 
+    print("ghg")
+#nested if 
+b=6
+if b>3:
+    print("hii")
+    if b<7:
+        print("hii")
+
+#using or
+age=19
+if age==18 or age>18:
+    print("eligible")
+else :
+    print("no")
+
+
+
+
+   
