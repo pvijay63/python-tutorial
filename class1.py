7b34a4bc09f573259f1b3fafa114d76b3edf7b02
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-11-02 11:57:10 +0530

    example for class and object in oop

---
+++ class1.py
@@ -0,0 +1,11 @@
+#class good():
+    #class body
+   # b=good()#object creation
+class bike():
+    a=4
+    def ji(self):#self is keyword used to print the output in a method
+        print(self.a)
+b=bike()
+v=bike()
+b.ji()
+v.ji()
