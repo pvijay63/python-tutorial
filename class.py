00308651240c9cba1fe07cfe1b1aa313a453a20b
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-11-02 11:56:28 +0530

    class and object creation in oop

---
+++ class.py
@@ -0,0 +1,23 @@
+#class in oop
+'''
+it is template
+blueprint of an object
+'''
+#syntax
+class bike():
+    #class is keyword
+    #bike(): is class definition
+#object
+''''
+it is physical entity
+we can create any no of objects for a class
+memory is allocated when object is created
+''''
+#syntax
+#objectname=classname()
+'using object we can access methods and variables of a class'
+#syntax:
+#  object name.method()
+#objectname.variable
+
+
