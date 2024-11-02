a4802b88cf3b520c5a6b01ffec8d30288aca6ed8
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-11-02 11:46:54 +0530

    error handling in python

---
+++ error_handle.py
@@ -0,0 +1,26 @@
+#error handling
+'''
+interrupting the normal execution of a code is error or exception
+ to handle that errors we use error handling
+'''
+#error handlings
+'''
+try: #used to solve error incase of future
+ risky code
+except:#incase of error present case
+    print('error')
+else:#incase of no error in try block
+    print('no error')
+finally:#it is always prints in anycase
+    print('always')
+'''
+#a=1
+#print(b)
+try:
+    print(b)
+except:
+    print('error')
+else:
+    print('no error')
+finally:
+    print('always')
