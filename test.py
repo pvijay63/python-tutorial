6a5ecf15933eac2b01bec2cd5586127b4d8f0bf2
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2024-11-08 19:59:30 +0530

    all over concepts of python .

--- test.py
+++ test.py
@@ -1,9 +1,46 @@
-a="hello"
-b=9.9
-c=9+8j
-print(type(a))
-print(type(b))
-print(type(c))
+#typeconvertion,implicity-convert one type to other with no dataloss,explicity--convert one type toothe with dataloss
+#int(),float(),bool(),string(),omplex()
+#types of variables ,global or static variable,instance or local variable
+#set is muatable,
+#add(),update(),remove(),pop()
+#set operations, intersection(),union(),difference(),issubset(),issuperset()
+#dictionary,key value pair called an item,key is immutable,values mutable
+#dictionary methods,get(),keys(),update(),values()
+#CONDITion ,if ,if else,nested if ,nnested if else
+#loops for,while
+#jumpstatements ,break,continue,pass
+#list ,mutable
+#append,extend([]),pop,index,insert(5,6),remove,slicing,count
+#tuple,immutable
+#concat,min max,sum,len,repetation,iteration,membership opera(in,not in),identity(is,is not)
+#string ,methods--lower,upper,startswith,endswith,find,index,split,rstrip,lstrip,count,remove,removeprefix,removesuffix,replace
+#functions,defining function,orbitary arguements,keyword arguements
+#nested functions
+#using funcions of one program in another program by import ---call by function
+#filehandling,open,read/write,close
+#file operations
+'''
+r-read
+w-write
+a-write
+r+-read write
+w+-wite read
+'''
+#errorhandling orexception handling,try block,except block,else block
+#class is a template ,object is a used to call methods and variables
+#inheritence ,single,multilevel,multiple,hierachical inheritences
+#Encappsulation,private,protected,public access specifiers
+#polymorphism--method overloading,method overriding
+
+
+
+
+
+
+
+
+
+
 
 
 
